1f33e1dcad8bbe75277927911dc2c9b5927cf9f7
floweryclover <floweryclover@gmail.com>
2024-06-28 17:02:20 +0900

    OmokServer Handler와 ClientRpc partial class로 분할

--- OmokServer.ClientRpc.cs
+++ OmokServer.ClientRpc.cs
@@ -12,62 +12,6 @@ namespace WebOmokServer
         /// 
         /// </summary>
         /// <param name="client"></param>
-        /// <param name="message"></param>
-        /// <returns></returns>
-        /// <exception cref="JsonException"></exception>
-        /// <exception cref="MessageSizeOverFlowException"></exception>
-        private async Task HandleMessageAsync(Client client, string message)
-        {
-            using JsonDocument jsonDocument = JsonDocument.Parse(message);
-            JsonElement rootElement = jsonDocument.RootElement;
-            JsonElement messageName = rootElement.GetProperty("msg");
-
-            var messageNameString = messageName.GetString();
-            switch (messageNameString)
-            {
-                case "createRoom":
-                    {
-                        string roomName = rootElement.GetProperty("roomName").GetString() ?? "";
-                        if (roomName.Length == 0)
-                        {
-                            await ClientFlashMessageAsync(client, "방 이름을 입력해 주세요.", FlashMessageType.Error);
-                            return;
-                        }
-                        foreach (var gameRoom in _gameRooms)
-                        {
-                            if (gameRoom.State == GameRoom.RoomState.Inactive)
-                            {
-                                gameRoom.RoomName = roomName;
-                                if (gameRoom.AddPlayer(client.Id))
-                                {
-                                    foreach (var peer in _clients.Values)
-                                    {
-                                        await ClientSendRoomItemAsync(peer, gameRoom.RoomId);
-                                    }
-                                }
-                                return;
-                            }
-                        }
-                        await ClientFlashMessageAsync(client, "최대 생성할 수 있는 방의 개수를 초과했습니다.", FlashMessageType.Error);
-                        return;
-                    }
-                case "requestAllRoomDatas":
-                    {
-                        await ClientSendAllRoomItemsAsync(client);
-                        return;
-                    }
-                default:
-                    {
-                        Console.WriteLine($"[알 수 없는 메시지 수신]: {messageNameString}");
-                        return;
-                    }
-            }
-        }
-
-        /// <summary>
-        /// 
-        /// </summary>
-        /// <param name="client"></param>
         /// <param name="jsonObject"></param>
         /// <returns></returns>
         /// <exception cref="MessageSizeOverFlowException"></exception>
