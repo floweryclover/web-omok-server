e77a18a65284da7d5385ca7dbdca8173f3811f58
floweryclover <floweryclover@gmail.com>
2024-06-28 14:16:33 +0900

    asd

---
+++ GameRoom.cs
@@ -0,0 +1,190 @@
+﻿using System.Security.Cryptography.X509Certificates;
+
+namespace WebOmokServer
+{ 
+	public class GameRoom
+	{
+		public class ClientIdArgs : EventArgs
+		{
+			public string ClientId { get; set; }
+
+			public ClientIdArgs(string clientId)
+			{
+				ClientId = clientId;
+			}
+		}
+
+		public enum RoomState
+		{
+			Inactive,
+			Waiting,
+			Playing,
+		}
+		public int RoomId { get; }
+		private RoomState _state;
+		public RoomState State { get { return _state; } }
+		private string _roomName;
+		public string RoomName
+		{
+			get
+			{
+				return _roomName;
+			}
+			set 
+			{
+				_roomName = value;
+				RoomNameChanged?.Invoke(this, new EventArgs());
+			} 
+		}
+
+		private string? _roomOwnerId;
+		public string? RoomOwnerId { get { return _roomOwnerId; } }
+
+		private void _playerAssign(ref string? target, string? newValue)
+		{
+			if (target != null)
+			{
+				PlayerKicked?.Invoke(this, new ClientIdArgs(target));
+			}
+			target = newValue;
+		}
+		private string? _blackPlayer;
+		private string? BlackPlayer {
+			get
+			{
+				return _blackPlayer;
+			}
+			set
+			{
+				_playerAssign(ref _blackPlayer, value);
+				if (value == null)
+				{
+					if (WhitePlayer != null)
+					{
+						_roomOwnerId = WhitePlayer;
+						RoomOwnerChanged?.Invoke(this, new ClientIdArgs(WhitePlayer));
+					}
+                    else
+                    {
+                        _roomOwnerId = null;
+                    }
+                }
+			}
+		}
+
+		private string? _whitePlayer;
+		private string? WhitePlayer
+		{
+			get
+			{
+				return _whitePlayer;
+			}
+			set
+			{
+				_playerAssign(ref _whitePlayer, value);
+				if (value == null)
+				{
+					if (BlackPlayer != null)
+					{
+						_roomOwnerId = BlackPlayer;
+						RoomOwnerChanged?.Invoke(this, new ClientIdArgs(BlackPlayer));
+					}
+					else
+					{
+						_roomOwnerId = null;
+					}
+				}
+			}
+		}
+
+		public event EventHandler<ClientIdArgs>? PlayerKicked;
+		public event EventHandler? GameRoomInactivated;
+		public event EventHandler<ClientIdArgs>? RoomOwnerChanged;
+		public event EventHandler? RoomNameChanged;
+
+		public GameRoom(int roomId)
+		{
+			RoomId = roomId;
+			_roomName = "같이 게임해요";
+			_blackPlayer = null;
+			_whitePlayer = null;
+			_roomOwnerId = null;
+		}
+
+		public bool AddPlayer(string playerId)
+		{
+			if (State == RoomState.Playing)
+			{
+				return false;
+			}
+
+			if (_blackPlayer != null && _whitePlayer != null)
+			{
+				return false;
+			}
+			else if (_blackPlayer == null && _whitePlayer == null)
+			{
+				_state = RoomState.Waiting;
+				var random = new Random();
+				bool isBlack = random.Next(1) == 0;
+				
+				if (isBlack)
+				{
+					_blackPlayer = playerId;
+				}
+				else
+				{
+					_whitePlayer = playerId;
+				}
+				_roomOwnerId = playerId;
+			}
+			else
+			{
+				if (_blackPlayer == null)
+				{
+					_blackPlayer = playerId;
+				}
+				else
+				{
+					_whitePlayer = playerId;
+				}
+			}
+			return true;
+		}
+
+		public void RemovePlayer(string playerId)
+		{
+			bool someoneLeft = false;
+			if (BlackPlayer != null & BlackPlayer == playerId)
+			{
+				BlackPlayer = null;
+				someoneLeft = true;
+			}
+			else if (WhitePlayer != null & WhitePlayer == playerId)
+			{
+				WhitePlayer = null;
+				someoneLeft = true;
+			}
+
+			if (someoneLeft)
+			{
+				if (BlackPlayer == null && WhitePlayer == null)
+				{
+					InactivateRoom();
+				}
+			}
+		}
+
+		public void InactivateRoom()
+		{
+			BlackPlayer = null;
+			WhitePlayer = null;
+			GameRoomInactivated?.Invoke(this, new EventArgs());
+		}
+
+		public bool IsPlayerJoined(string playerId)
+		{
+			return (WhitePlayer != null && WhitePlayer == playerId) || (BlackPlayer != null && BlackPlayer == playerId);
+		}
+	}
+}
--- OmokServer.cs
+++ OmokServer.cs
@@ -17,16 +17,59 @@ namespace WebOmokServer
             {
             }
         }
-        private Dictionary<string, WebSocket> _webSockets = new Dictionary<string, WebSocket>();
-        private const int BUFFER_SIZE = 256;
 
-        enum FlashMessageType { INFO, WARNING, ERROR }
+        private const int MAX_ROOM_COUNT = 16;
+		private const int BUFFER_SIZE = 256;
+
+        private GameRoom[] _gameRooms = new GameRoom[MAX_ROOM_COUNT];
+        private Dictionary<string, string> _nicknames = new Dictionary<string, string>();
+        
+		private Dictionary<string, WebSocket> _webSockets = new Dictionary<string, WebSocket>();
+
+        enum FlashMessageType { Info, Warning, Error }
+
+        public OmokServer()
+        {
+            for (int roomId = 0; roomId<MAX_ROOM_COUNT; roomId++)
+            {
+                var newRoom = new GameRoom(roomId);
+                newRoom.PlayerKicked += OnPlayerKicked;
+                newRoom.GameRoomInactivated += OnGameRoomInactivated;
+                _gameRooms[roomId] = newRoom;
+            }
+        }
+
+        private void OnGameRoomInactivated(object? sender, EventArgs args)
+        {
+            foreach (var client in _webSockets.Values)
+            {
+                ClientRemoveRoomItemAsync(client, ((GameRoom)sender).RoomId).Wait();
+            }
+        }
+
+        private void OnPlayerKicked(object? sender, GameRoom.ClientIdArgs args)
+        {
+            if (!_webSockets.ContainsKey(args.ClientId))
+                return;
+            ClientNotifyKickedFromGameRoomAsync(_webSockets[args.ClientId]).Wait();
+        }
+
+        private void OnRoomNameChanged(object? sender, EventArgs args)
+        {
+			foreach (var client in _webSockets.Values)
+			{
+				ClientSendRoomItemAsync(client, ((GameRoom)sender).RoomId).Wait();
+			}
+		}
 
         public async Task HandleClientAsync(WebSocket client, ConnectionInfo connectionInfo)
         {
-            var clientAddressInfo = string.Format($"클라이언트 {connectionInfo.Id}({connectionInfo.RemoteIpAddress.MapToIPv4()}:{connectionInfo.RemotePort})");
+            var clientAddressInfo = string.Format($"클라이언트 {connectionInfo.Id}({connectionInfo.RemoteIpAddress?.MapToIPv4()}:{connectionInfo.RemotePort})");
             Console.WriteLine($"[접속] {clientAddressInfo}");
+            _webSockets.Add(connectionInfo.Id, client);
+            _nicknames.Add(connectionInfo.Id, "플레이어");
             var buffer = new byte[BUFFER_SIZE];
+            
             try
             {
 				while (client.State == WebSocketState.Open)
@@ -45,7 +88,7 @@ namespace WebOmokServer
                     }
 					string message = Encoding.UTF8.GetString(buffer, 0, result.Count);
                     Console.WriteLine("메시지 크기: " + result.Count);
-                    await HandleMessageAsync(client, clientAddressInfo, message);
+                    await HandleMessageAsync(client, connectionInfo.Id, clientAddressInfo, message);
 				}
 			}
             catch (WebSocketException webSocketException)
@@ -77,9 +120,18 @@ namespace WebOmokServer
                 }
             }
 			Console.WriteLine($"[접속 해제] {clientAddressInfo}");
+            _webSockets.Remove(connectionInfo.Id);
+            _nicknames.Remove(connectionInfo.Id);
+            foreach (var gameRoom in _gameRooms)
+            {
+                if (gameRoom.IsPlayerJoined(connectionInfo.Id))
+                {
+                    gameRoom.RemovePlayer(connectionInfo.Id);
+                }
+            }
 		}
 
-        private async Task HandleMessageAsync(WebSocket client, string clientAddressInfo, string message)
+        private async Task HandleMessageAsync(WebSocket client, string clientId, string clientAddressInfo, string message)
         {
             using JsonDocument jsonDocument = JsonDocument.Parse(message);
             JsonElement rootElement = jsonDocument.RootElement;
@@ -90,8 +142,33 @@ namespace WebOmokServer
             {
                 case "createRoom":
                     {
-                        var roomName = rootElement.GetProperty("roomName").GetString();
-                        await ClientFlashMessageAsync(client, roomName, FlashMessageType.INFO);
+                        string roomName = rootElement.GetProperty("roomName").GetString() ?? "";
+                        if (roomName.Length == 0)
+                        {
+                            await ClientFlashMessageAsync(client, "방 이름을 입력해 주세요.", FlashMessageType.Error);
+                            return;
+                        }
+                        foreach (var gameRoom in _gameRooms)
+                        {
+                            if (gameRoom.State == GameRoom.RoomState.Inactive)
+                            {
+                                gameRoom.RoomName = roomName;
+                                if (gameRoom.AddPlayer(clientId))
+                                {
+                                    foreach (WebSocket webSocket in _webSockets.Values)
+                                    {
+                                        await ClientSendRoomItemAsync(webSocket, gameRoom.RoomId);
+                                    }
+                                }
+                                return;
+                            }
+                        }
+                        await ClientFlashMessageAsync(client, "최대 생성할 수 있는 방의 개수를 초과했습니다.", FlashMessageType.Error);
+                        return;
+                    }
+                case "requestAllRoomDatas":
+                    {
+                        await ClientSendAllRoomItemsAsync(client);
                         return;
                     }
                 default:
@@ -105,9 +182,9 @@ namespace WebOmokServer
         private async Task ClientFlashMessageAsync(WebSocket client, string message, FlashMessageType messageType)
         {
             int messageTypeNumber;
-            if (messageType == FlashMessageType.INFO)
+            if (messageType == FlashMessageType.Info)
                 messageTypeNumber = 0;
-            else if (messageType == FlashMessageType.WARNING)
+            else if (messageType == FlashMessageType.Warning)
                 messageTypeNumber = 1;
             else
                 messageTypeNumber = 2;
@@ -117,6 +194,58 @@ namespace WebOmokServer
                 text = message,
                 flashType = messageTypeNumber
             };
+            await SendClientAsync(client, messageObject);
+        }
+
+        private async Task ClientSendAllRoomItemsAsync(WebSocket client)
+        {
+            foreach (var gameRoom in _gameRooms)
+            {
+                await ClientSendRoomItemAsync(client, gameRoom.RoomId);
+            }
+        }
+
+        private async Task ClientRemoveRoomItemAsync(WebSocket client, int roomId)
+        {
+            var messageObject = new
+            {
+                msg = "removeRoomItem",
+                roomId = roomId
+            };
+            await SendClientAsync(client, messageObject);
+        }
+
+        private async Task ClientSendRoomItemAsync(WebSocket client, int roomId)
+        {
+            if (roomId < 0 || roomId >= MAX_ROOM_COUNT)
+            {
+                return;
+            }
+
+            GameRoom gameRoom = _gameRooms[roomId];
+            string roomOwnerName = "알 수 없음";
+            if (_nicknames.Contains)
+			if (gameRoom.State == GameRoom.RoomState.Waiting)
+			{
+				var messageObject = new
+				{
+					msg = "sendRoomItem",
+					roomId = gameRoom.RoomId,
+					roomName = gameRoom.RoomName,
+					roomOwner = _nicknames[gameRoom.RoomOwnerId ?? ""]
+				};
+                
+				await SendClientAsync(client, messageObject);
+			}
+		}
+
+        private async Task ClientNotifyKickedFromGameRoomAsync(WebSocket client)
+        {
+            var messageObject = new
+            {
+                msg = "kickedFromGameRoom"
+            };
+
             await SendClientAsync(client, messageObject);
         }
 
